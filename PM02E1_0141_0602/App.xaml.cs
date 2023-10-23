abaf83db8e550143c0814610954d89377fb92f82
l_alv <l_alv@DESKTOP-42DI0BD>
2023-10-22 22:08:20 -0600

    Agregar archivos de proyecto.

---
+++ PM02E1_0141_0602/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using PM02E1_0141_0602.Controllers;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM02E1_0141_0602
+{
+    public partial class App : Application
+    {
+
+        static DataBase db;
+
+        public static DataBase DBase
+        {
+            get
+            {
+                //se declara la ruta donde estan los archivos de la BD
+                if (db == null)
+                {
+                    String FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "EmpleDB.db3");//db3 es extencion de sqlite
+                    db = new DataBase(FolderPath);
+                }
+
+                return db;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PM02E1_0141_0602/Controllers/DataBase.cs
@@ -0,0 +1,59 @@
+﻿using PM02E1_0141_0602;
+using PM02E1_0141_0602.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM02E1_0141_0602.Controllers
+{
+    public class DataBase
+    {
+        readonly SQLiteAsyncConnection dbase;
+
+        public DataBase(string dbpath)
+        {
+            dbase = new SQLiteAsyncConnection(dbpath);
+
+            /*Se crean las tablas*/
+            dbase.CreateTableAsync<Models.sitios>();
+
+        }
+
+        public Task<int> SitioSave(sitios sitio)
+        {
+            if (sitio.id != 0)//update del registro
+            {
+                return dbase.UpdateAsync(sitio);
+            }
+            else
+            {
+                return dbase.InsertAsync(sitio);//inserter nuevo registro
+            }
+        }
+
+        public Task<List<sitios>> getListSitio()
+        {
+            return dbase.Table<sitios>().ToListAsync();//se convierte el resultado a una lista.
+        }
+
+        public async Task<sitios> getSitio(int pid)
+        {
+            return await dbase.Table<sitios>()//se usa explesion lamba
+                .Where(i => i.id == pid)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<int> DeleteSitio(sitios sitio)
+        {
+            return await dbase.DeleteAsync(sitio);
+        }
+
+        public static implicit operator InternalDataCollectionBase(DataBase v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
